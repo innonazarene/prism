b4c621d585282c37d6f0a1bc9029d19b8064b4de
Rustom Pedales <pedales.rustom@gmail.com>
2023-02-19 12:02:15 +0800

    Update InitBackEndDev.php

--- app/Console/Commands/InitBackEndDev.php
+++ app/Console/Commands/InitBackEndDev.php
@@ -33,9 +33,8 @@ class InitBackEndDev extends Command
     {
         parent::__construct();
 
+
     }
-
-
 	protected $path = '';
 	protected $packages =  [
 		'kitloong/laravel-migrations-generator',
@@ -54,50 +53,66 @@ class InitBackEndDev extends Command
      */
     public function handle()
     {
+		$tables = $this->getDatabaseTables();
+		$this->cleanFiles();
+		$this->checkAndInstallPackage();
+        $this->migrateDatabase();
+		$this->startSeeding($tables,env('DB_DATABASE'));
+		$this->makeCoreRequest();
+		$this->generateModelsAndControllers($tables);
+        $this->updateApiRoutes($tables,'v1');
+		Artisan::call('route:clear');
 
-		$routes_content = '';
-		$routes_use_content = '';
 
-		//check the packages if exist
-		$composerLockFile = File::get(base_path('composer.lock'));
-		foreach($this->packages as $package)
-		{
 
-			if (strpos($composerLockFile, $package) == false) {
-				exec("composer require --dev $package");
-			}
-		}
-		//Migrate Database using this packages : https://github.com/kitloong/laravel-migrations-generator
-		shell_exec('rm -rf database/migrations/*');
-		shell_exec('start cmd.exe @cmd /k "php artisan migrate:generate --squash --skip-log  & exit"');
-		echo PHP_EOL.'Done:Migration';
+    }
 
+	private function getDatabaseTables()
+	{
 		$tables = DB::select('SHOW TABLES');
         $databaseName = config('database.connections.'.config('database.default').'.database');
         $tables = collect($tables);
         $tables = $tables->pluck('Tables_in_'.$databaseName);
+		return $tables;
+	}
 
+	private function checkAndInstallPackage()
+	{
+		//check the packages if exist
+		$composerLockFile = File::get(base_path('composer.lock'));
+		foreach($this->packages as $package)
+		{
+			if (strpos($composerLockFile, $package) == false) {
+				exec("composer require --dev $package");
+			}
+		}
+	}
 
-		// Check if the web.backup.php file exists
-		if (!File::exists(base_path('routes/web.backup.php'))) {
-			// Get the contents of the web.php file
-			$webFileContents = File::get(base_path('routes/web.php'));
+	private function migrateDatabase()
+	{
+		//Migrate Database using this packages : https://github.com/kitloong/laravel-migrations-generator
+		shell_exec('start cmd.exe @cmd /k "php artisan migrate:generate --squash --skip-log & exit"');
+		echo PHP_EOL.'Done:Migration';
+	}
 
-			// Create a backup of the web.php file
-			File::put(base_path('routes/web.backup.php'), $webFileContents);
-		}else{
-			// Delete the web.php file
-			File::delete(base_path('routes/web.php'));
-
-			// Copy the web.backup.php file to web.php
-			File::copy(base_path('routes/web.backup.php'), base_path('routes/web.php'));
+	private function backupOrRestoreFile($filePath, $backupFilePath)
+	{
+		if (!File::exists($backupFilePath)) {
+			$fileContents = File::get($filePath);
+			File::put($backupFilePath, $fileContents);
+			echo PHP_EOL.'Done: add'.$filePath.' to '.$backupFilePath;
+		} else {
+			File::delete($filePath);
+			File::copy($backupFilePath, $filePath);
+			echo PHP_EOL.'Done: restore'.$filePath.' to '.$backupFilePath;
 		}
 
 
-        foreach($tables as $table)
+	}
+	private function generateModelsAndControllers($tables)
+	{
+		foreach($tables as $table)
         {
-			$webFileContents = File::get(base_path('routes/web.php'));
-
 			//model and controller
             if($table === 'migrations') continue;
             $className = Str::camel(Str::singular($table));
@@ -108,42 +123,125 @@ class InitBackEndDev extends Command
 			//edit content
 			$body = '';
 			$basePath = base_path('App\\Http\\Controllers\\'.$className.'Controller.php');
-			$haystack = file_get_contents($basePath);
-			$haystack = str_replace('}', '', $haystack);
-			$body .= '{'.PHP_EOL.'    //Index'.PHP_EOL.'    public function index()'.PHP_EOL.'    {'.PHP_EOL.'        return '.$className.'::with([])->take(1000)->get();'.PHP_EOL.'    }'.PHP_EOL;
+			$ctrlContent = file_get_contents($basePath);
+			$ctrlContent = str_replace('}', '', $ctrlContent);
+			$body .= '{'.PHP_EOL.'    //Index Example : http://127.0.0.1:8000/api/{{PREFIX}}/{{API-ROUTE}}/?with={{WITH-FUNCTION(found in models)}}&'.PHP_EOL.'    //orderBy={{FIELD-NAME}:{{ASC or DESC}}&limit={{LIMIT}}&fields={{FIELD}}&filter={{FILTER-FIELD}}:{{VALUE}}'.PHP_EOL.'    public function index(Request $request)'.PHP_EOL.'    {'.PHP_EOL.'        return $this->displayRequest(NULL,$request,new '.$className.');'.PHP_EOL.'    }'.PHP_EOL;
 			$body .= ''.PHP_EOL.'    //create'.PHP_EOL.'    public function create()'.PHP_EOL.'    {'.PHP_EOL.'        '.PHP_EOL.'    }'.PHP_EOL;
 			$body .= ''.PHP_EOL.'    //store'.PHP_EOL.'    public function store(Request $request)'.PHP_EOL.'    {'.PHP_EOL.'        return '.$className.'::create($request->all());'.PHP_EOL.'    }'.PHP_EOL;
-			$body .= ''.PHP_EOL.'    //show'.PHP_EOL.'    public function show($id)'.PHP_EOL.'    {'.PHP_EOL.'        return '.$className.'::findOrFail($id);'.PHP_EOL.'    }'.PHP_EOL;
+			$body .= ''.PHP_EOL.'    //show Example : http://127.0.0.1:8000/api/{{PREFIX}}/{{API-ROUTE}}/{{ID}}?with={{WITH-FUNCTION(found in models)}}&'.PHP_EOL.'    //orderBy={{FIELD-NAME}:{{ASC or DESC}}&limit={{LIMIT}}&fields={{FIELD}}&filter={{FILTER-FIELD}}:{{VALUE}}'.PHP_EOL.'    public function show(Request $request, $id)'.PHP_EOL.'    {'.PHP_EOL.'        return $this->displayRequest($id,$request,new '.$className.');'.PHP_EOL.'    }'.PHP_EOL;
 			$body .= ''.PHP_EOL.'    //edit'.PHP_EOL.'    public function edit($id)'.PHP_EOL.'    {'.PHP_EOL.'        '.PHP_EOL.'    }'.PHP_EOL;
 			$body .= ''.PHP_EOL.'    //update'.PHP_EOL.'    public function update(Request $request, $id)'.PHP_EOL.'    {'.PHP_EOL.'        '.PHP_EOL.'        $data = '.$className.'::findOrFail($id);'.PHP_EOL.'        $data->update($request->all());'.PHP_EOL.'        return $data;'.PHP_EOL.''.PHP_EOL.'    }'.PHP_EOL;
 			$body .= ''.PHP_EOL.'    //destroy'.PHP_EOL.'    public function destroy($id)'.PHP_EOL.'    {'.PHP_EOL.'        '.PHP_EOL.'        $data = '.$className.'::findOrFail($id);'.PHP_EOL.'        $data->delete();'.PHP_EOL.'        return $data;'.PHP_EOL.''.PHP_EOL.'    }'.PHP_EOL.PHP_EOL.'}';
-			$haystack = str_replace('{', $body, $haystack);
+			$ctrlContent = str_replace('{', $body, $ctrlContent);
 
-			$haystack = str_replace($body, $body, $haystack);
-			if (strpos($haystack, $this->needles['table'].PHP_EOL.'use App\\Models\\'.$className.';') == false) {
-				$haystack = str_replace($this->needles['table'], $this->needles['table'].PHP_EOL.'use App\\Models\\'.$className.';', $haystack);
+			$ctrlContent = str_replace($body, $body, $ctrlContent);
+			if (strpos($ctrlContent, $this->needles['table'].PHP_EOL.'use App\\Models\\'.$className.';') == false) {
+				$ctrlContent = str_replace($this->needles['table'], $this->needles['table'].PHP_EOL.'use App\\Models\\'.$className.';', $ctrlContent);
 			}
-
-			file_put_contents($basePath, $haystack);
-			$routes_content .= PHP_EOL."Route::Resource('".strtolower($className)."', ".$className."Controller::class);";
-			$routes_use_content .= PHP_EOL.'use App\Http\Controllers\\'.$className.'Controller;';
-
+			file_put_contents($basePath, $ctrlContent);
 			echo PHP_EOL.'Done:'.$className.'[model && controller]';
+
 		}
-		$webFileContents = str_replace('<?php','<?php'.PHP_EOL.$routes_use_content, $webFileContents);
-		echo PHP_EOL.'Done: Added WebFileContents -> routes use';
-		$webFileContents = str_replace('});','});'.PHP_EOL.PHP_EOL.$routes_content, $webFileContents);
-		echo PHP_EOL.'Done: Added WebFileContents -> routes content';
+		echo PHP_EOL.'Done: add Controller and Models';
 
-		file_put_contents(base_path('routes/web.php'), $webFileContents);
-		Artisan::call('route:clear');
+	}
 
-
-		foreach($this->packages as $package)
+	private function updateApiRoutes($tables,$prefix)
+	{
+		$routes_content = PHP_EOL.'Route::prefix("'.$prefix.'")->group(function(){';
+		$routes_use_content = '';
+		foreach($tables as $table)
 		{
-			if (strpos($composerLockFile, $package) != false) {
-				exec("composer remove $package");
+			if($table === 'migrations') continue;
+			$className = Str::camel(Str::singular($table));
+			$className = ucfirst($className);
+			$apiFileContents = File::get(base_path('routes/api.php'));
+			$routes_content .= PHP_EOL."    Route::Resource('".strtolower($className)."', ".$className."Controller::class);";
+			$routes_use_content .= PHP_EOL.'use App\Http\Controllers\\'.$className.'Controller;';
+		}
+		$apiFileContents = str_replace('<?php','<?php'.PHP_EOL.$routes_use_content, $apiFileContents);
+		echo PHP_EOL.'Done: Added apiFileContents -> routes use';
+		$apiFileContents = str_replace('});','});'.PHP_EOL.PHP_EOL.$routes_content.PHP_EOL."});", $apiFileContents);
+		echo PHP_EOL.'Done: Added apiFileContents -> routes content';
+
+		file_put_contents(base_path('routes/api.php'), $apiFileContents);
+		echo PHP_EOL.'Done: add API-Routes';
+
+	}
+
+	private function startSeeding($tables,$prefix)
+	{
+		foreach($tables as $table)
+		{
+			Artisan::call('iseed '.$table.' --classnameprefix='.$prefix);
+			echo PHP_EOL.'Done: Seeding - '.$table;
+		}
+	}
+
+	private function makeCoreRequest()
+	{
+		$strUpUse = 'use Illuminate\Routing\Controller as BaseController;';
+		$coreCtrlContent = file_get_contents(base_path('public/backup/Controller.backup.php'));
+		$content = "    public \$requestRules = [
+		'with' => 'string',
+		'orderBy' =>'string',
+		'limit' => 'integer',
+		'fields' => 'string',
+		'filter' => 'string'
+	];
+	public function displayRequest(\$id='',Request \$request, \$model)
+	{
+		\$validatedData = \$request->validate(\$this->requestRules);
+		\$query = \$model->query();
+		if(\$id != ''){
+			\$query->findOrFail(\$id);
+		}
+		if (isset(\$validatedData['with'])) {
+			\$query->with(explode(',', \$validatedData['with']));
+		}
+		if (isset(\$validatedData['orderBy'])) {
+			\$orderByParams = explode(',', \$validatedData['orderBy']);
+			foreach (\$orderByParams as \$orderByParam) {
+				\$orderByArray = explode(':', \$orderByParam);
+				\$query->orderBy(\$orderByArray[0], \$orderByArray[1] ?? 'asc');
 			}
 		}
-    }
+		if (isset(\$validatedData['filter'])) {
+			\$filterParams = explode(',', \$validatedData['filter']);
+			foreach (\$filterParams as \$filterParam) {
+				\$filterArray = explode(':', \$filterParam);
+				\$query->where(\$filterArray[0], \$filterArray[1]);
+			}
+		}
+		if (isset(\$validatedData['limit'])) {
+			\$query->limit(\$validatedData['limit']);
+		}
+		if (isset(\$validatedData['fields'])) {
+			\$query->select(explode(',', \$validatedData['fields']));
+		}
+		\$results = \$query->get();
+		return response()->json(\$results);
+	}";
+		$content = str_replace('	','    ',$content);
+		$coreCtrlContent = str_replace('}',$content.PHP_EOL."}", $coreCtrlContent);
+		$coreCtrlContent = str_replace($strUpUse,$strUpUse.PHP_EOL."use Illuminate\Http\Request;", $coreCtrlContent);
+		file_put_contents(base_path('app/Http/Controllers/Controller.php'), $coreCtrlContent);
+		echo PHP_EOL.'Done: add function in Core core-controller';
+	}
+
+
+	private function cleanFiles()
+	{
+		$this->backupOrRestoreFile(base_path('routes/api.php'), base_path('public/backup/api.backup.php'));
+		$this->backupOrRestoreFile(base_path('app/Http/Controllers/Controller.php'), base_path('public/backup/Controller.backup.php'));
+		if(!is_dir(base_path('public/backup')))
+		{
+			mkdir(base_path('public/backup'));
+			echo PHP_EOL.'Done: Make Directory Backup';
+
+		}else{
+			shell_exec('rm -rf app/models/* & rm -rf database/migrations/* & rm -rf app/Http/Controllers/*');
+		}
+		echo PHP_EOL.'Done: Clean Files';
+	}
 }
